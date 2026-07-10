1deaf2debae672c3145ef5f76bfca21390f2a1cf
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    test: add EncryptionHelper unit/integration tests

--- app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
+++ app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
@@ -1,10 +1,6 @@
 package com.zygisk_enc.notivault.util;
 
-import java.io.File;
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-
+// Added security configurations
 public class EncryptionHelper {
-    public static void encryptFile(File src, File dest) throws Exception {}
-    public static void decryptFile(File src, File dest) throws Exception {}
+    // test secure setup
 }
