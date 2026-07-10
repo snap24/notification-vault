97c45bb4611ad0ce4cf542e699b06b1b71736f28
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    feat: implement AES/GCM decryption for string data

--- app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
+++ app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
@@ -1,8 +1,14 @@
 package com.zygisk_enc.notivault.util;
 
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import java.security.KeyStore;
 import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class EncryptionHelper {
     private static final String TRANSFORMATION = "AES/GCM/NoPadding";
     private static final String KEY_ALIAS = "notivault_secure_key";
+    private static final String ANDROID_KEYSTORE = "AndroidKeyStore";
 }
