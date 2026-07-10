f22e5521df8008274293969be202a2abb07917de
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    feat: implement file encryption utility methods

--- app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
+++ app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
@@ -1,44 +1,13 @@
 package com.zygisk_enc.notivault.util;
 
-import android.security.keystore.KeyGenParameterSpec;
-import android.security.keystore.KeyProperties;
-import java.security.KeyStore;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
+import javax.crypto.CipherInputStream;
 import javax.crypto.SecretKey;
 
 public class EncryptionHelper {
-    private static final String TRANSFORMATION = "AES/GCM/NoPadding";
-    private static final String KEY_ALIAS = "notivault_secure_key";
-    private static final String ANDROID_KEYSTORE = "AndroidKeyStore";
-
-    private static synchronized SecretKey getOrCreateKey() throws Exception {
-        KeyStore keyStore = KeyStore.getInstance(ANDROID_KEYSTORE);
-        keyStore.load(null);
-        if (keyStore.containsAlias(KEY_ALIAS)) {
-            return (SecretKey) keyStore.getKey(KEY_ALIAS, null);
-        }
-        return generateNewKey();
-    }
-
-    private static SecretKey generateNewKey() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE);
-        keyGenerator.init(new KeyGenParameterSpec.Builder(KEY_ALIAS,
-                KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
-                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
-                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
-                .build());
-        return keyGenerator.generateKey();
-    }
-
-    private static void handleKeyInvalidated() {
-        try {
-            KeyStore keyStore = KeyStore.getInstance(ANDROID_KEYSTORE);
-            keyStore.load(null);
-            keyStore.deleteEntry(KEY_ALIAS);
-            generateNewKey();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void encryptFile(File src, File dest) throws Exception {
     }
 }
