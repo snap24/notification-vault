c7c2b48cc179333d810443b6470aeb349a53af82
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    feat: create AppRuleDao interfaces

--- app/src/main/java/com/zygisk_enc/notivault/database/AppRuleEntity.java
+++ app/src/main/java/com/zygisk_enc/notivault/database/AppRuleEntity.java
@@ -1,2 +1,27 @@
 package com.zygisk_enc.notivault.database;
-public class AppRuleEntity {}
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "app_rules")
+public class AppRuleEntity {
+    @PrimaryKey
+    @NonNull
+    public String packageName;
+    
+    public String appName;
+    public boolean blockAll;
+    public String blockKeywords;
+    public String allowKeywords;
+    public boolean isRuleEnabled;
+
+    public AppRuleEntity(@NonNull String packageName, String appName, boolean blockAll, String blockKeywords, String allowKeywords, boolean isRuleEnabled) {
+        this.packageName = packageName;
+        this.appName = appName;
+        this.blockAll = blockAll;
+        this.blockKeywords = blockKeywords != null ? blockKeywords : "";
+        this.allowKeywords = allowKeywords != null ? allowKeywords : "";
+        this.isRuleEnabled = isRuleEnabled;
+    }
+}
