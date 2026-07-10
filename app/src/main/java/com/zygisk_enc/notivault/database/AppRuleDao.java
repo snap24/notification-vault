0edfc32beb4e6b89bbd47028940328c5ad66aa52
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    layout: design app rules dialog configurations

--- app/src/main/java/com/zygisk_enc/notivault/database/AppRuleDao.java
+++ app/src/main/java/com/zygisk_enc/notivault/database/AppRuleDao.java
@@ -1,2 +1,34 @@
 package com.zygisk_enc.notivault.database;
-public interface AppRuleDao {}
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import java.util.List;
+
+@Dao
+public interface AppRuleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(AppRuleEntity rule);
+
+    @Delete
+    void delete(AppRuleEntity rule);
+
+    @Query("DELETE FROM app_rules WHERE packageName = :packageName")
+    void deleteByPackage(String packageName);
+
+    @Query("SELECT * FROM app_rules WHERE packageName = :packageName LIMIT 1")
+    AppRuleEntity getRuleSync(String packageName);
+
+    @Query("SELECT * FROM app_rules WHERE packageName = :packageName LIMIT 1")
+    LiveData<AppRuleEntity> getRule(String packageName);
+
+    @Query("SELECT * FROM app_rules ORDER BY appName ASC")
+    LiveData<List<AppRuleEntity>> getAllRules();
+
+    @Query("SELECT * FROM app_rules ORDER BY appName ASC")
+    List<AppRuleEntity> getAllRulesSync();
+}
