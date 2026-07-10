099397de7c7fcb3907f79710a0fb1e82ac0eaf2a
zygisk-enc <chinmai.enc@gmail.com>
2026-07-10 13:43:57 +0530

    feat: implement file decryption utility methods

--- app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
+++ app/src/main/java/com/zygisk_enc/notivault/util/EncryptionHelper.java
@@ -1,13 +1,10 @@
 package com.zygisk_enc.notivault.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
 import javax.crypto.SecretKey;
 
 public class EncryptionHelper {
-    public static void encryptFile(File src, File dest) throws Exception {
-    }
+    public static void encryptFile(File src, File dest) throws Exception {}
+    public static void decryptFile(File src, File dest) throws Exception {}
 }
